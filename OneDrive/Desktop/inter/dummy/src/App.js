a271870dd2369435fc23c15bfb8962f7fa8da03c
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-02-06 13:41:11 +0530

    class

---
+++ OneDrive/Desktop/inter/dummy/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Greet from "./Components/Greet";
+import Welcome from "./Components/GreetClass";
+
+
+function App() {
+  return (
+    <div>
+      <Greet/>
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ OneDrive/Desktop/inter/dummy/src/Components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Greet = () => {
+  return (
+    <>
+      <h1>Hello From Greet.</h1>
+    </>
+  );
+};
+
+export default Greet;
